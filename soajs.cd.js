af7cdb8e17928731653d73a6d36a69c1cdeec3eb
antoinehage <me@antoinehage.com>
2018-12-07 15:34:34 -0500

    enhanced version

--- soajs.cd.js
+++ soajs.cd.js
@@ -97,10 +97,10 @@ var utils = {
 		params.json = true;
 		
 		try {
-			config = require("./soa.js");
+			config = require("./soajs.json");
 		}
 		catch (e) {
-			console.log("Could not find a soa.js file, searching for custom config file [config.js] ...");
+			console.log("Could not find a soajs.json file, searching for custom config file [config.js] ...");
 			
 			try {
 				config = require('./config.js');
